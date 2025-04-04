ae1e31e742aa2f5f71f1e65585077f471aa4794f
Matias Nicolas Arregui <matiasarregui2@gmail.com>
2025-04-04 13:21:31 -0300

    agregado a views (cargos, categorias)

--- seguridadVial/views.py
+++ seguridadVial/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, HttpResponseRedirect, redirect, get_object_or_404
 from django.contrib import messages
 from .models import Cargo, Categoria, Elementos, Institucion, InstitucionPersona, Persona
-from .forms import PersonaForm, CargoForm, CategoriaForm, AuthenticationForm, InstitucionForm, InstitucionPersonaForm, InstitucionPersonaFormSet
+from .forms import PersonaForm, CargoForm, CategoriaForm, AuthenticationForm, InstitucionForm, InstitucionPersonaForm, InstitucionPersonaFormSet, ElementosForm
 from django.urls import reverse, reverse_lazy
 from django.contrib.auth.views import LoginView
 from django.db.models import ProtectedError
@@ -38,26 +38,76 @@ class PersonaBorrar(DeleteView):
     #Ver Elementos
 class listaElemento(ListView):
     model = Elementos
-    template_name = os.path.join("listas", "listaPersonas.html")
-    context_object_name = 'personas'
+    template_name = os.path.join("listas", "listaElementos.html")
+    context_object_name = 'elementos'
     paginate_by = 7
-    #Crear persona
-class PersonaNueva(CreateView):
-    model = Persona
-    form_class = PersonaForm
-    template_name = os.path.join("formularios", "personaForm.html")
-    success_url = reverse_lazy('listaPersonas')
-    #Modificar persona
-class PersonaModificar(UpdateView):
-    model = Persona
-    form_class = PersonaForm
-    template_name = os.path.join("formularios", "personaForm.html")
-    success_url = reverse_lazy('listaPersonas')
-    #Borrar persona
-class PersonaBorrar(DeleteView):
-    model = Persona
-    template_name = os.path.join("confirmacionBorrado", "personaBorrar.html")
-    success_url = reverse_lazy('listaPersonas')
+    #Crear elemento
+class ElementoNuevo(CreateView):
+    model = Elementos
+    form_class = ElementosForm
+    template_name = os.path.join("formularios", "elementoForm.html")
+    success_url = reverse_lazy('listaElementos')
+    #Modificar Elemento
+class ElementoModificar(UpdateView):
+    model = Elementos
+    form_class = ElementosForm
+    template_name = os.path.join("formularios", "elementoForm.html")
+    success_url = reverse_lazy('listaElementos')
+    #Borrar Elemento
+class ElementoBorrar(DeleteView):
+    model = Elementos
+    template_name = os.path.join("confirmacionBorrado", "elementoBorrar.html")
+    success_url = reverse_lazy('listaElementos')
+
+# Categoria Views ------------------------------------------------------------------------------>
+    #Ver categorias
+class listaCategoria(ListView):
+    model = Categoria
+    template_name = os.path.join("listas", "listaCategorias.html")
+    context_object_name = 'categorias'
+    paginate_by = 7
+    #Crear categoria
+class CategoriaNueva(CreateView):
+    model = Categoria
+    form_class = CategoriaForm
+    template_name = os.path.join("formularios", "categoriaForm.html")
+    success_url = reverse_lazy('listaCategorias')
+    #Modificar categoria
+class CategoriaModificar(UpdateView):
+    model = Categoria
+    form_class = CategoriaForm
+    template_name = os.path.join("formularios", "categoriaForm.html")
+    success_url = reverse_lazy('listaCategorias')
+    #Borrar categoria
+class CategoriaBorrar(DeleteView):
+    model = Categoria
+    template_name = os.path.join("confirmacionBorrado", "categoriaBorrar.html")
+    success_url = reverse_lazy('listaCategorias')
+
+# Categoria Views ------------------------------------------------------------------------------>
+    #Ver categorias
+class listaCargos(ListView):
+    model = Cargo
+    template_name = os.path.join("listas", "listaCargos.html")
+    context_object_name = 'cargos'
+    paginate_by = 7
+    #Crear categoria
+class CargoNuevo(CreateView):
+    model = Cargo
+    form_class = CargoForm
+    template_name = os.path.join("formularios", "cargoForm.html")
+    success_url = reverse_lazy('listaCargos')
+    #Modificar categoria
+class CargoModificar(UpdateView):
+    model = Cargo
+    form_class = CargoForm
+    template_name = os.path.join("formularios", "cargoForm.html")
+    success_url = reverse_lazy('listaCargos')
+    #Borrar categoria
+class CargoBorrar(DeleteView):
+    model = Cargo
+    template_name = os.path.join("confirmacionBorrado", "cargoBorrar.html")
+    success_url = reverse_lazy('listaCargos')
 
 
 
