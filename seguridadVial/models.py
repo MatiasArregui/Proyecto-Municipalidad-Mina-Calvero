961a9ff0ca577f8b1f9e560daf41916de68f7073
Matias Nicolas Arregui <matiasarregui2@gmail.com>
2025-06-08 19:55:22 -0300

    proteccion CASCADE/SET

--- seguridadVial/models.py
+++ seguridadVial/models.py
@@ -1,11 +1,21 @@
 from django.db import models
 
 # Funcion de remplazo del set, les dejo el ejemplo
-# def seleccionar_mesa_alternativa():
-#     # Buscamos el mozo comun o base que remplazara a los demas
-#     mesa_alternativa = Mesa.objects.filter(nombre__exact="Mesa predeterminada").first()
-#     if mesa_alternativa:
-#         return mesa_alternativa
+def seleccionar_categoria_remplazo():
+    # Buscamos el mozo comun o base que remplazara a los demas
+    categoria_alternativa = Categoria.objects.filter(nombre__exact="seleccionar categoria").first()
+    if categoria_alternativa:
+        return categoria_alternativa
+def seleccionar_persona_remplazo():
+    # Buscamos el mozo comun o base que remplazara a los demas
+    persona_alternativa = Persona.objects.filter(nombre__exact="seleccionar persona").first()
+    if persona_alternativa:
+        return persona_alternativa
+def seleccionar_cargo_remplazo():
+    # Buscamos el mozo comun o base que remplazara a los demas
+    cargo_alternativo = Cargo.objects.filter(nombre__exact="selecccionar cargo").first()
+    if cargo_alternativo:
+        return cargo_alternativo
 
 
 
@@ -16,9 +26,9 @@ class Elementos(models.Model):
     cantidad = models.IntegerField(default=1, null=True, blank=True)
     # aca vamos a tener que poner un SET con un predeterminado
     # ej:  id_cliente = models.ForeignKey(Cliente, on_delete=models.SET(seleccionar_cliente_alternativo))
-    id_categoria = models.ForeignKey('Categoria', on_delete=models.CASCADE)
+    id_categoria = models.ForeignKey('Categoria', on_delete=models.SET(seleccionar_categoria_remplazo))
     estado = models.BooleanField(default=False, null=True, blank=True)
-    id_persona = models.ForeignKey('Persona', on_delete=models.CASCADE)
+    id_persona = models.ForeignKey('Persona', on_delete=models.SET(seleccionar_persona_remplazo))
     
     def __str__(self):
         return self.nombre
@@ -45,6 +55,8 @@ class Categoria(models.Model):
     
 #     def __str__(self):
 #         return self.nombre
+
+
 class Institucion(models.Model):
     nombre = models.CharField(max_length=120, null=True, blank=True)
     mail = models.CharField(max_length=120, default="", null=True, blank=True)
@@ -55,7 +67,7 @@ class Institucion(models.Model):
     # Campo para seleccionar una sola persona (representante)
     id_persona_encargado = models.ForeignKey(
         'Persona',
-        on_delete=models.CASCADE,
+        on_delete=models.SET(seleccionar_persona_remplazo),
         related_name='encargado'
     )
     
@@ -87,7 +99,7 @@ class Persona(models.Model):
     telefono = models.CharField(max_length=30, default="",null=True, blank=True)
     # aca vamos a tener que poner un SET con un predeterminado
     # ej:  id_cliente = models.ForeignKey(Cliente, on_delete=models.SET(seleccionar_cliente_alternativo))
-    id_cargo = models.ForeignKey('Cargo', on_delete=models.CASCADE)
+    id_cargo = models.ForeignKey('Cargo', on_delete=models.SET(seleccionar_cargo_remplazo))
     
     def __str__(self):
         return self.nombre
