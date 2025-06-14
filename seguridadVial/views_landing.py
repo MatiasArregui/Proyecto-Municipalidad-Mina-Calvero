8b4687bee3e5314bc0c36dd2c57d3f049ae6a6c8
Matias Nicolas Arregui <matiasarregui2@gmail.com>
2025-06-14 10:50:54 -0300

    bases de landing page

---
+++ seguridadVial/views_landing.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render, HttpResponseRedirect, redirect, get_object_or_404
+from django.contrib import messages
+from .models import Cargo, Categoria, Elementos, Institucion, InstitucionPersona, InstitucionElemento, Persona
+from .forms import PersonaForm, CargoForm, CategoriaForm, ElementosForm, AuthenticationForm, InstitucionForm, InstitucionPersonaForm, InstitucionPersonaFormSet, InstitucionElementoForm, InstitucionElementoFormSet
+from django.urls import reverse, reverse_lazy
+from django.contrib.auth.views import LoginView
+from django.db.models import ProtectedError
+from django.views.generic import ListView, CreateView, DeleteView, UpdateView
+import os
+
+def landingPage(request):
+    return render(request, template_name= os.path.join("landingPage", "pagPrincipal.html"), context=None)
+def catastrofes(request):
+    return render(request, template_name= os.path.join("landingPage", "catastrofes.html"), context=None)
+def protocolosEmergencia(request):
+    return render(request, template_name= os.path.join("landingPage", "protocolos.html"), context=None)
+def integrantesDefCivil(request):
+    return render(request, template_name= os.path.join("landingPage", "areas.html"), context=None)
+def mapa(request):
+    return render(request, template_name= os.path.join("landingPage", "mapa.html"), context=None)
